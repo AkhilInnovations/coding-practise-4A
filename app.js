7669fe32ae5047356d826395e162718487fce8b8
Akhil Bolla <akhilbolla89@gmail.com>
2023-01-31 06:36:41 +0000

    coding-practise-4A

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 
 const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
 
 const path = require("path");
-const dbPath = path.join(__dirname, "goodreads.db");
+const dbPath = path.join(__dirname, "cricketTeam.db");
 
 let db = null;
 
@@ -15,8 +16,8 @@ const initializeDBAndServer = async () => {
       filename: dbPath,
       driver: sqlite3.Database,
     });
-    app.listen(3000, () => {
-      console.log("Server Running at http://localhost:3000/");
+    app.listen(3003, () => {
+      console.log("Server Running at http://localhost:3003/");
     });
   } catch (e) {
     console.log(`DB Error: ${e.message}`);
@@ -27,7 +28,28 @@ initializeDBAndServer();
 
 app.get("/players/", async (request, response) => {
   const getBooksQuery = `
-    SELECT * FROM cricketTeam`;
+    SELECT * FROM cricket_team`;
   const booksArray = await db.all(getBooksQuery);
   response.send(booksArray);
+  //   console.log(booksArray);
+});
+
+module.exports = app;
+
+//post API2
+app.post("/players/", async (request, response) => {
+  const playerDetails = request.body;
+  const { player_id, player_name, jersey_number, role } = playerDetails;
+  const addBooksQuery = `
+    INSERT INTO 
+        cricket_team (player_id, player_name, jersey_number, role)
+    VALUES (
+        ${player_id},
+        ${player_name},
+        ${jersey_number},
+        ${role}
+    ); `;
+  const dbResponse = await db.run(addBooksQuery);
+  const playerID = dbResponse.lastID;
+  response.send("Player Added to Team");
 });
